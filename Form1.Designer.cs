214cce86832b704f9a8c74fdd4f8ec47205f05a8
Gabriele Viganò <gabriviga2005@gmail.com>
2023-10-17 15:20:58 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace _22._35
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.cmbM = new System.Windows.Forms.ComboBox();
+            this.cmbT = new System.Windows.Forms.ComboBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.lbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_22._35.Properties.Resources._2;
+            this.pictureBox1.Location = new System.Drawing.Point(185, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(458, 152);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(185, 170);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 21);
+            this.cmb1.TabIndex = 5;
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Location = new System.Drawing.Point(285, 211);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 21);
+            this.cmb2.TabIndex = 6;
+            // 
+            // cmbM
+            // 
+            this.cmbM.FormattingEnabled = true;
+            this.cmbM.Location = new System.Drawing.Point(407, 170);
+            this.cmbM.Name = "cmbM";
+            this.cmbM.Size = new System.Drawing.Size(121, 21);
+            this.cmbM.TabIndex = 7;
+            // 
+            // cmbT
+            // 
+            this.cmbT.FormattingEnabled = true;
+            this.cmbT.Location = new System.Drawing.Point(497, 211);
+            this.cmbT.Name = "cmbT";
+            this.cmbT.Size = new System.Drawing.Size(121, 21);
+            this.cmbT.TabIndex = 8;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(329, 268);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(152, 38);
+            this.btn.TabIndex = 9;
+            this.btn.Text = "Stampa resistenza";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(344, 354);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(119, 25);
+            this.lbl.TabIndex = 10;
+            this.lbl.Text = "Resistenza";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.cmbT);
+            this.Controls.Add(this.cmbM);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.ComboBox cmbM;
+        private System.Windows.Forms.ComboBox cmbT;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label lbl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _22._35
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cmb 1 cifra
+            cmb1.Items.Add("Nero");
+            cmb1.Items.Add("Marrone");
+            cmb1.Items.Add("Rosso");
+            cmb1.Items.Add("Arancione");
+            cmb1.Items.Add("Giallo");
+            cmb1.Items.Add("Verde");
+            cmb1.Items.Add("Blu");
+            cmb1.Items.Add("Viola");
+            cmb1.Items.Add("Grigio");
+            cmb1.Items.Add("Bianco");
+            //Cmb 2 cifra
+            cmb2.Items.Add("Nero");
+            cmb2.Items.Add("Marrone");
+            cmb2.Items.Add("Rosso");
+            cmb2.Items.Add("Arancione");
+            cmb2.Items.Add("Giallo");
+            cmb2.Items.Add("Verde");
+            cmb2.Items.Add("Blu");
+            cmb2.Items.Add("Viola");
+            cmb2.Items.Add("Grigio");
+            cmb2.Items.Add("Bianco");
+            //Cmb moltiplicatore
+            cmbM.Items.Add(1);
+            cmbM.Items.Add(10);
+            cmbM.Items.Add(100);
+            cmbM.Items.Add(1000);
+            cmbM.Items.Add(10000);
+            cmbM.Items.Add(100000);
+            cmbM.Items.Add(1000000);
+            //Cmb tolleranza
+            cmbT.Items.Add("Oro");
+            cmbT.Items.Add("Argento");
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            string R;
+            int c1;
+            int c2;
+            int M;
+            string T = "";
+            c1 = cmb1.SelectedIndex;
+            c2 = cmb2.SelectedIndex;
+            M = (int) cmbM.SelectedItem;
+            if(cmbT.SelectedIndex == 0)
+            {
+                T = "5 %";
+            }
+            else if(cmbT.SelectedIndex == 1)
+            {
+                T = "10 %";
+            }
+            R = (float.Parse(c1.ToString() + c2.ToString()) * M).ToString() + " Ω " + T;
+            lbl.Text = R;
+        }
+    }
+}
